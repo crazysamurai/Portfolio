f9e1c02146ebfad6a0c33d5e3c01eb5267520f0b
Gaurav-Supehia <79050860+Gaurav-Supehia@users.noreply.github.com>
2021-08-27 21:04:47 +0530

    started making portfolio

---
+++ script/script.js
@@ -0,0 +1,72 @@
+//navbar
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
+
+//typewriter
+
+const textDisplay = document.getElementById("text");
+const phrases = ["Web Developer", "Technical Writer", "Casual Gamer"];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+
+function loop() {
+  isEnd = false;
+  textDisplay.innerHTML = currentPhrase.join("");
+
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i === phrases.length) {
+        i = 0;
+      }
+    }
+  }
+  //controling the speed of typing
+  const spedUp = Math.random() * (80 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 200;
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+  setTimeout(loop, time);
+}
+
+loop();
