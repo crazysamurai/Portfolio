e4106b08f3619260e67f8b3f8c65723625233e44
Gaurav-Supehia <79050860+Gaurav-Supehia@users.noreply.github.com>
2021-08-29 00:04:02 +0530

    fixed a bug with scroll

--- script/script.js
+++ script/script.js
@@ -71,10 +71,12 @@ function loop() {
 
 loop();
 
-scroll;
+// scroll
 const faders = document.querySelectorAll(".fade-in");
-const appearOptions = { threshold: 1 };
-rootMargin: "-100px 0px 0px 0px";
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
 //observer
 const appearOnScroll = new IntersectionObserver(function (
   entries,
