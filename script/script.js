18db9a404df8e68f803f7b8abc7f4746dd17b7f4
Gaurav-Supehia <79050860+Gaurav-Supehia@users.noreply.github.com>
2021-08-28 23:11:13 +0530

    responsive edits

--- script/script.js
+++ script/script.js
@@ -70,3 +70,27 @@ function loop() {
 }
 
 loop();
+
+scroll;
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = { threshold: 1 };
+rootMargin: "-100px 0px 0px 0px";
+//observer
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
